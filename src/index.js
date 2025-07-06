fc10e6cf5fe817292f9059fdd8f61618f6d16745
noumi553 <noumanaziz383@gmail.com>
2025-07-06 07:31:31 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ChatBord from './chat-bord/ChatBord';
-//const root = ReactDOM.createRoot(document.getElementById('root'));
+
 createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-      <ChatBord/>
+      <ChatBord />
    </React.StrictMode>
 );
 
