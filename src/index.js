3664cbd932ca5d8cb40ed35d54637d78716133cb
noumi553 <iamtayyabkhan568@gmail.com>
2025-07-06 21:47:36 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ChatBord from './chat-bord/ChatBord';
 
 createRoot(document.getElementById('root')).render(
-   <React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
       <ChatBord />
-   </React.StrictMode>
+    </HashRouter>
+  </React.StrictMode>
 );
 
 reportWebVitals();
